0a144129ddf9dcb896a77c9d032350a4a7375fbb
prometheus <prometheus@prometheuss-MBP.home>
2020-02-14 00:20:13 -0500

    added API key

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,7 +6,7 @@ class SearchesController < ApplicationController
 
 
    def search
-     @resp = Faraday.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,BSV,LTC,USDT,EOS&tsyms=USD')
+     @resp = Faraday.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,BSV,LTC,USDT,EOS&tsyms=USD&api_key=8bdf39f3712f726d76c9af792c462ab19b4d5b9ce53fbb349b076e70c7f44fb2')
      @body = JSON.parse(@resp.body)
 
         if @resp.success?
